f81e24fc09245d2827d8dda8b4ff148ec6385dae
Rafa Gómez <dotzecker@gmail.com>
2014-10-16 12:47:45 +0200

    Sexy...

---
+++ main.js
@@ -0,0 +1,37 @@
+console.log('\n¡¡DEJA DE COTILLEAR!! ᕦ( ͡°╭͜ʖ╮͡° )ᕤ\n');
+console.log('rafa.sexy es una marca registrada de http://twitter.com/dotzecker\n');
+
+console.log('Va... Te explico un poco de qué va la cosa...');
+console.table([{
+    'En qué': 'Ser sexy',
+    'Quien': 'Rafa',
+    'Es el mejor': '✔'
+}, {
+    'En qué': 'Ser awesome',
+    'Quien': 'Rafa',
+    'Es el mejor': '✔'
+}, {
+    'En qué': 'Ser temido',
+    'Quien': 'Rafa',
+    'Es el mejor': '✔'
+}, {
+    'En qué': 'Ser amado',
+    'Quien': 'Rafa',
+    'Es el mejor': '✔'
+}, {
+    'En qué': 'Ser un loser',
+    'Quien': 'Tú',
+    'Es el mejor': '✔'
+}, {
+    'En qué': 'Ser un troll',
+    'Quien': 'Rafa',
+    'Es el mejor': '✘ (lo es @SergiGP)'
+}]);
+
+console.log('\nVees, soy mejor que tú y yo no me rodeo con la plebe, así que cierra ya la web!!');
+
+console.log('\nTambién podéis ver que soy un mago.js');
+console.log('Y un sexy.js');
+console.log('Y un genial.js');
+console.log('Y un perfecto.js');
+console.log('Y un k4rlikyNoSabe.js');
