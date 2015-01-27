38dbeca474c40276f6caed8172079e0c2d7c21e7
Rafa Gómez <dotzecker@gmail.com>
2015-01-27 18:01:20 +0100

    Spring is coming

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ console.log('\n¡¡DEJA DE COTILLEAR!! ᕦ( ͡°╭͜ʖ╮͡° )ᕤ\n');
 console.log('rafa.sexy es una marca registrada de http://twitter.com/dotzecker\n');
 
 console.log('Va... Te explico un poco de qué va la cosa...');
+
 console.table([{
     'En qué': 'Ser sexy',
     'Quien': 'Rafa',
@@ -28,10 +29,11 @@ console.table([{
     'Es el mejor': '✘ (lo es @SergiGP)'
 }]);
 
+console.log('Si no has visto nada recarga la página con esto abierto! O:');
+
 console.log('\nVees, soy mejor que tú y yo no me rodeo con la plebe, así que cierra ya la web!!');
 
 console.log('\nTambién podéis ver que soy un mago.js');
-console.log('Y un sexy.js');
-console.log('Y un genial.js');
-console.log('Y un perfecto.js');
-console.log('Y un k4rlikyNoSabe.js');
+console.log('* y un sexy.js');
+console.log('* y un genial.js');
+console.log('* y un perfecto.js');
